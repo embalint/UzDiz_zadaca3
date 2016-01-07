33c31b6efb5556123a28e3a390352576edd8820a
Emil <embalint@foi.hr>
2016-01-07 17:40:53 +0100

    FileScanner fix
    FileScanner fix

--- src/scanner/FileScanner.java
+++ src/scanner/FileScanner.java
@@ -9,7 +9,9 @@ import controller.Controller;
 import controller.InputController;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
@@ -24,36 +26,51 @@ import registry.Registry;
  * @author Emil
  */
 public class FileScanner extends Controller {
-
+    File actual = new File((String) this.get("filepath"));
     @Override
     public void work() {
+        
 
-        ArrayList<String> buffer = new ArrayList<>();
-        File actual = new File((String) this.get("filepath"));
-        for (File f : actual.listFiles()) {
+    
+
+    ArrayList<String> buffer = new ArrayList<>();
+    
+    
+    for (File f: actual.listFiles () 
+        ) {
 
             BasicFileAttributes attributes;
-            try {
-                attributes = Files.readAttributes(Paths.get(f.getPath()), BasicFileAttributes.class);
+        try {
+            attributes = Files.readAttributes(Paths.get(f.getPath()), BasicFileAttributes.class);
 
-                FileTime creTime = attributes.creationTime();
-                FileSystemScannerAtributes attr = new FileSystemScannerAtributes();
+            FileTime creTime = attributes.creationTime();
+            FileSystemScannerAtributes attr = new FileSystemScannerAtributes();
 
-                attr.setFileSize(attributes.size());
-                attr.setCreationTime(creTime.toString());
-                attr.setFileName(f.getName());
-                attr.setModifiedTime(attributes.lastModifiedTime().toString());
-                attr.setFiletype(Files.probeContentType(Paths.get(f.getPath())));
+            attr.setFileSize(attributes.size());
+            attr.setCreationTime(creTime.toString());
+            attr.setFileName(f.getName());
+            attr.setModifiedTime(attributes.lastModifiedTime().toString());
+            attr.setFiletype(Files.probeContentType(Paths.get(f.getPath())));
 
-                buffer.add(attr.toString());
+            buffer.add(attr.toString());
 
-            } catch (IOException ex) {
-                Logger.getLogger(InputController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
+        } catch (IOException ex) {
+            Logger.getLogger(InputController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        ((Model) Registry.getInstance().get("model.structure")).setBuffer(buffer);
 
+    }
+
+     (
+
+(Model) Registry.getInstance().get("model.structure")).setBuffer(buffer);
+
+    }
+    
+    
+    private static boolean isDirEmpty(final Path actual) throws IOException {
+        try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(actual)) {
+            return !dirStream.iterator().hasNext();
+        }
     }
 
 }
